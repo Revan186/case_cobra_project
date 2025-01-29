131ad2e98a3e239c88fe420ec7b367cba399ad78
Revan186 <aragas1245@gmail.com>
2025-01-29 15:41:37 +0400

    fuck cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  crossOrigin: 'anonymous',
+  async headers() {
+    return [
+      {
+        source: "/api/auth/[kindeAuth]",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://case-cobra-official.vercel.app", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "next-router-prefetch",
+          },
+        ],
+      },
+    ];
+  },
 	images: {
 		domains: ["ufs.sh", "utfs.io", 'f83u96zfqk.ufs.sh'],
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
